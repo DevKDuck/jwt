d0d69edb92ab154ea9f7c909afee8298a64aa1f1
DevKDuck <loading95@naver.com>
2024-11-06 10:55:02 +0900

    jwt를 위한 security 설정

---
+++ src/main/java/com/devkduck/jwt/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.devkduck.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); //서버 응답시 json을 자바스크립트에서 처리할 수 있게 할지 설정
+		config.addAllowedOriginPattern("*"); //모든 ip 응답 허용
+		config.addAllowedHeader("*"); // 모든 header 응답 허용
+		config.addAllowedMethod("*"); //모든 메서드(put,delete 등)에 응답 허용
+		source.registerCorsConfiguration("/api/**", config);
+		return new CorsFilter(source);
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/devkduck/jwt/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.devkduck.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	
+	private final CorsFilter corsFilter;
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		return http.authorizeHttpRequests(
+                authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer
+                        .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "MANAGER", "ADMIN") //인증만 되면 들어가도록 함
+                        .requestMatchers("/api/v1/manager/**").hasAnyRole("MANAGER", "ADMIN") //자동으로 ROLE_접두어 생성해줌
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN") //hasAnyRole은 하나라도 가지고 있으면 hasRole은 한가지
+                        .anyRequest().permitAll()
+        )
+        .formLogin(login -> login.disable())
+        .httpBasic(basic -> basic.disable())
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilter(corsFilter) // @CrossOrigin 인증이 없을때 , 인증이 있을때 필터에 등록 
+        .build();
+		
+		
+	}
+	
+	//session stateless , form login, httpbasic 사용안함
+}
